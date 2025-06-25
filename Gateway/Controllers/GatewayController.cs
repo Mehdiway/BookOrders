7327f0e2fa7b301fbebf687f63b628618c5ce6af
Mehdi Armachi <marmachi@sqli.com>
2025-06-25 16:28:47 +0100

    Added API Gateway

---
+++ Gateway/Controllers/GatewayController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gateway.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class GatewayController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Default()
+    {
+        return Ok("Gateway running...");
+    }
+}
